2948a78ba63d7c40101799638105bdf67806a6f4
boom1999 <45328326+boom1999@users.noreply.github.com>
2020-07-15 08:58:06 +0800

    manual_select  debug

--- manual_select.cpp
+++ manual_select.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<string.h>
 #include<iomanip>
 #include<cstdlib>
 #include<ctime>
@@ -15,11 +16,13 @@ int guess (int num) {
 
 int manual_select () {
     cout << "-------------------------------------------------------" << endl;
-    cout << "-              欢迎进入手动选题界面                      -" << endl;
+    cout << "-              欢迎进入手动选题界面                   -" << endl;
     cout << "-------------------------------------------------------" << endl << endl;
 
     cout << "输入任意数字继续(-1返回菜单)：";
     match_stu = 0;
+    int* match = new int[ real_stu ];
+    memset (match, 0, real_stu);
     int flag;
     cin >> flag;
 
@@ -32,23 +35,35 @@ int manual_select () {
             match_stu--;
             continue;
         }
+        cout << "----------------------------------" << endl;
         cout << "请输入学号：";
         string en_xh; cin >> en_xh;
         int s = 0;//查找是否存在该学号
         for(int i = 0; i < real_stu; i++) {
             if(stu[ i ].get_xh () == en_xh) {
+                if(match[i]==1) {
+                    s = 1;
+                    cout << "该学号已匹配,请重新输入!" << endl;
+                    cout << "----------------------------------" << endl << endl;
+                    match_stu--;
+                    break;
+                }
+                match[ i ] = 1;
                 s = 1;
                 stu[ i ].in_su (str_res);
                 sub[ res ].in_stu (en_xh);
                 cout << "匹配完成！" << endl;
+                cout << "----------------------------------" << endl << endl;
             }
         }
         if(s == 0) {
             cout << "定位失败！请确认输入内容是否正确并重新输入！" << endl;
+            cout << "----------------------------------" << endl << endl;
             match_stu--;
         }
     }
     cout << endl;
+    delete[]match;
     cout << "输入题目信息完成" << endl;
     cout << "输入任意数字返回菜单：";
     int re_num;
